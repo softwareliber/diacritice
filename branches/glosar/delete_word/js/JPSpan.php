f613708a87622da4dce3850777f3b995a3208021
lucian.grijincu <lucian.grijincu@b5ffaacc-f233-11dd-a295-0b7b0935bf94>
2009-02-19 03:35:12 +0000

    added branch 'delete_word' -- copy of 'trunk'

---
+++ branches/glosar/delete_word/js/JPSpan.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @package JPSpan
+* @version $Id: JPSpan.php,v 1.1.1.1 2005/04/24 21:11:49 dand Exp $
+*/
+/**
+* Set the path constant
+*/
+if ( !defined('JPSPAN') ) {
+    define('JPSPAN', dirname(__FILE__) . '/JPSpan/');
+}
+
+
