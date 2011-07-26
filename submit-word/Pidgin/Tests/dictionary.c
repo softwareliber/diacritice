750b5acb3fafe80c8ae7241f644e408e7428c5b9
chelaru.laura91@gmail.com <chelaru.laura91@gmail.com@b5ffaacc-f233-11dd-a295-0b7b0935bf94>
2011-07-26 17:51:05 +0000

    Plug-inul cu incercarile de detectare de cuvant

--- submit-word/Pidgin/Tests/dictionary.c
+++ submit-word/Pidgin/Tests/dictionary.c
@@ -16,15 +16,40 @@
 
 static guint signal_id;
 static gulong hook_id;
+static PidginConversation *curconv;
 
-static void menu_cb (GtkWidget *item, gpointer data) {
+
+static gboolean
+get_word (PidginConversation *gtkconv)
+{
+    GtkIMHtml *imhtml = GTK_IMHTML (gtkconv->imhtml);
+    /*GtkTextBuffer *buffer = imhtml->text_buffer;
+    GtkTextIter start, *pstart = &start, end, *pend = &end;
+    gchar *pBuf = "";*/
+
+    /*if ( gtk_text_buffer_get_selection_bounds ( buffer, pstart, pend )) {
+        GtkTextBuffer *tmpbuf = gtk_text_buffer_new(gtk_text_buffer_get_tag_table(buffer));
+
+        gtk_text_buffer_get_bounds(tmpbuf, pstart, pend);
+        pBuf = g_strdup_printf("%s", gtk_text_buffer_get_text(tmpbuf, pstart, pend, FALSE));
+    }*/
+    //pBuf = "Aoleu!";
+    return TRUE;
+}
+
+static gboolean menu_cb (GtkWidget *item, gpointer data) {
     PurplePlugin *plugin = data;
-    purple_notify_message (purple_notify_get_handle(), PURPLE_NOTIFY_MSG_INFO, "test", "Boo! This is a test!", NULL, NULL, NULL);
+    PidginConversation *gtkconv;
+
+   // purple_signal_connect(purple_conversations_get_handle(), "conversation-created", G_CALLBACK(get_word), NULL);
+    //gchar *aux =get_word(gtkconv);
+
+    purple_notify_message (purple_notify_get_handle(), PURPLE_NOTIFY_MSG_INFO, "test", "aux", NULL, NULL, NULL);
 
     return TRUE;
 }
 
-static void add_menu_item_cb (GSignalInvocationHint *hint, guint n_params, const GValue *pvalues, gpointer data) {
+static gboolean add_menu_item_cb (GSignalInvocationHint *hint, guint n_params, const GValue *pvalues, gpointer data) {
 
     GtkTextView *view = GTK_TEXT_VIEW(g_value_get_object(pvalues));
     GtkWidget *menu, *item;
@@ -39,7 +64,7 @@ static void add_menu_item_cb (GSignalInvocationHint *hint, guint n_params, const
 
 	gtk_widget_show_all(item);
     g_signal_connect(G_OBJECT(item), "activate", G_CALLBACK(menu_cb), data);
-	gtk_menu_shell_insert(GTK_MENU_SHELL(menu), item, 0);
+	gtk_menu_shell_insert(GTK_MENU_SHELL(menu), item, 7);
 
 	return TRUE;
 }
@@ -51,14 +76,18 @@ static void init_plugin (PurplePlugin *plugin)
 
 static gboolean plugin_load (PurplePlugin *plugin)
 {
-    gpointer klass = NULL;
+   // gpointer klass = NULL;
 
-    klass = g_type_class_ref(GTK_TYPE_TEXT_VIEW);
+    //klass = g_type_class_ref(GTK_TYPE_TEXT_VIEW);
+
+    purple_signal_connect(purple_conversations_get_handle(),
+						"conversation-created",
+						plugin, PURPLE_CALLBACK(get_word), NULL);
 
     g_signal_parse_name("populate_popup", GTK_TYPE_TEXT_VIEW, &signal_id, NULL, FALSE);
 	hook_id = g_signal_add_emission_hook(signal_id, 0, add_menu_item_cb, plugin, NULL);
 
-    g_type_class_unref(klass);
+    //g_type_class_unref(klass);
 
     return TRUE;
 }
